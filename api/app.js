27703627607d1e3161b61ec2409570ffbdc9a982
Devang Ray <devangray624@gmail.com>
2022-05-31 17:25:58 -0400

    idk how this works

--- api/app.js
+++ api/app.js
@@ -32,11 +32,11 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 // app.use('/spotify', spotifyRouter)
 
+
 var client_id = process.env.clientID; // Your client id
 var client_secret = process.env.clientSecret; // Your secret
 var redirect_uri =  "http://localhost:3000/home"; // Your redirect uri
 var frontend_home = process.env.FRONTEND_HOME;
-
 var request = require("request"); // "Request" library
 var cors = require("cors");
 var querystring = require("querystring");
@@ -76,6 +76,101 @@ var querystring = require("querystring");
   );
 });
 
+app.get("/callback", function (req, res) {
+  console.log(req.body);
+  // // your application requests refresh and access tokens
+  // // after checking the state parameter
+  // console.log(req.query)
+  // var code = req.query.code || null;
+  // var state = req.query.state || null;
+  // var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  // // if (state === null || state !== storedState) {
+  // //   res.redirect(
+  // //     "/#" +
+  // //       querystring.stringify({
+  // //         error: "state_mismatch",
+  // //       })
+  // //   );
+  // // } else {
+  // //   res.clearCookie(stateKey);
+  // //   var authOptions = {
+  // //     url: "https://accounts.spotify.com/api/token",
+  // //     form: {
+  // //       code: code,
+  // //       redirect_uri: redirect_uri,
+  // //       grant_type: "authorization_code",
+  // //     },
+  // //     headers: {
+  // //       Authorization:
+  // //         "Basic " +
+  // //         new Buffer(client_id + ":" + client_secret).toString("base64"),
+  // //     },
+  // //     json: true,
+  // //   };
+
+  // //   request.post(authOptions, function (error, response, body) {
+  // //     if (!error && response.statusCode === 200) {
+  // //       var access_token = body.access_token,
+  // //         refresh_token = body.refresh_token;
+
+  // //       var options = {
+  // //         url: "https://api.spotify.com/v1/me",
+  // //         headers: { Authorization: "Bearer " + access_token },
+  // //         json: true,
+  // //       };
+
+  // //       // use the access token to access the Spotify Web API
+  // //       request.get(options, function (error, response, body) {
+  // //         console.log(body);
+  // //       });
+
+  // //       // we can also pass the token to the browser to make requests from there
+  // //       res.redirect(
+  // //         frontend_home +
+  // //           querystring.stringify({
+  // //             access_token: access_token,
+  // //             refresh_token: refresh_token,
+  // //           })
+  // //       );
+  // //     } else {
+  // //       res.redirect(
+  // //         "/#" +
+  // //           querystring.stringify({
+  // //             error: "invalid_token",
+  // //           })
+  // //       );
+  // //     }
+  // //   });
+  // // }
+});
+
+app.get("/refresh_token", function (req, res) {
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      Authorization:
+        "Basic " +
+        new Buffer(client_id + ":" + client_secret).toString("base64"),
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        access_token: access_token,
+      });
+    }
+  });
+});
 const PORT = process.env.PORT || 8888;
 console.log(`Listening on ${PORT}`);
 app.listen(PORT);
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,28 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react';
+
 export default function Home() {
+    const [data, setData] = useState();
+    var url = window.location.href;
+    url = url.replace("http://localhost:3000/home?code=", "")
+    var index  = url.indexOf("state")
+    var state = url.slice(index + 6);
+    var code = url.slice(0, index);
+
+    useEffect ( () => {
+        axios.post("http://localhost:9000/callback", {
+            state:state,
+            code:code
+          })
+        .then((res) => res.json())
+        .then((json) => setData(json))
+        .catch((err) => {console.log(err)})
+    }, [code])
     return(
         <>
             <p>You are home!</p>
+            <p>Code: {code}</p>
+            <p>State: {state}</p>
         </>
     );
 }
